8277e84a5ee04d72d16d2155170d757ad2a1c038
motsu13 <130522059+motsu13@users.noreply.github.com>
2024-12-09 18:47:05 +0900

    decelerateを定数に対応

--- vehicle.py
+++ vehicle.py
@@ -8,7 +8,7 @@ import setting
 変数:
 車両の位置
 車両の速度
-先行車両との車間距離
+先行車両との車間距離:gap
 先行車両の位置
 """
 
@@ -18,9 +18,14 @@ class Vehicle:
         車両の初期位置と初期速度を設定。
         position: 車両の初期位置（整数）
         speed: 車両の初期速度（0から最大速度までの整数）
+        preciding_position: 先行車両の位置
+        gap: 先行車両との車間距離(車両の前端と先行車両の後ろ端の間の距離)
         """
         self.position = position  # 現在の車両位置を保存
         self.speed = speed  # 現在の車両速度を保存
+        #以下1stepごとに更新が必要
+        self.preciding_position #= preciding_position
+        self.gap #= (preciding_position - position) - setting.Const().VEHICLE.LENGTH
 
 
     def move(self, new_position):
@@ -37,34 +42,21 @@ class Vehicle:
         if self.speed < setting.Const().MAX_SPEED: #車両の最大速度
             self.speed += 1
 
-    def deceleration(self,road,max_speed,boundary_condition):
+    def decelerate(self):
         """
         前の車両までの距離と自分の速度を比較し、距離が小さければ速度を減速。
+        gap:
         """
-        length_vehicle = 1#車両の全長
-        distance_next_vehicle = 1 # 次の車両までの車間距離
-
         #前方車両との車間距離を判断
-        if boundary_condition == 'open':
-            while (self.position + distance_next_vehicle) < len(road) and road[(self.position + distance_next_vehicle)] == -1:
-                distance_next_vehicle += 1
-                if distance_next_vehicle > max_speed:
-                    break
-            if (self.position + distance_next_vehicle) >= len(road):
-                #右端に着いたら車両を遠くに飛ばす
-                self.speed = len(road)#右辺は最大
-            
-            elif distance_next_vehicle <= self.speed:
-                #前の車両までの距離が最大速度以下の場合、その距離に減速する
-                self.speed = max(distance_next_vehicle - length_vehicle,0)
+        if self.gap < self.speed:
+            self.speed = self.gap
 
-    def random_decelerate(self, probability):
+    def random_decelerate(self):
         """
         指定された確率で車両をランダムに減速。
-        probability: 車両が減速する確率（0から1の範囲）
         """
-        if np.random.rand() < probability:
-            self.speed = max(0, self.speed - 1)  # 減速しても速度が負にならないようにする
+        if np.random.rand() < setting.Const().DECELERATION_PROBABILITY: #減速するかしないか
+            self.speed = max(0, self.speed - )  # 減速しても速度が負にならないようにする
 
 
 
