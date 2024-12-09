075cd14bc59807123e159240d0eb25a5c51c24e3
motsu13 <130522059+motsu13@users.noreply.github.com>
2024-12-09 22:21:36 +0900

    modelの原型を作成

--- nasch_model.py
+++ nasch_model.py
@@ -1,1 +1,30 @@
+"""
+Naschモデルを運用
+"""
+import vehicle
+import setting
 
+class NaschModel:
+    def __init__():
+        """
+        Naschモデルの初期化
+        """
+    
+    def setup_vehicles(self):
+        """
+        Vehicleインスタンスをリストとして生成
+        リストは車両が順番通り入っているものと考える
+        """
+
+    def update_vehicles(self):
+        """
+        （Naschモデルのルールに従い）各車両の位置と速度を更新。
+        """
+        
+            
+
+
+
+
+if __name__ == "__main__":
+    print()
