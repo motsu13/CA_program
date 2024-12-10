1ffeab5e17814a974958710d5f1d57f4934526d6
motsu13 <130522059+motsu13@users.noreply.github.com>
2024-12-10 14:40:47 +0900

    開放境界条件の時も完成?

--- boundary.py
+++ boundary.py
@@ -6,13 +6,21 @@ class BoundaryCondition:
     def __init__(self):
         pass
 
-    def check_condition(self,condition:str,vehicle):
+    def check_condition(self,condition:str,vehicles:list):
         """
         境界条件によって道路外に超えてしまった車両の変数を適切に変更する
         condition:境界条件(periodic,open)
         """
+        
         if condition == "periodic":
-            vehicle.position = vehicle.position % setting.Const().ROAD_LENGTH
+            for vehicle in vehicles:
+                vehicle.position = vehicle.position % setting.Const().ROAD_LENGTH
+        elif condition == "open":
+            #スライスにすることでvehiclesの削除をmainに適応することができる
+            vehicles[:] = [vehicle for vehicle in vehicles if vehicle.position < setting.Const().ROAD_LENGTH]
+        else:
+            print("境界条件の表記ミス")
+        
 
 if __name__ == "__main__":
     vehicles = []
@@ -28,7 +36,8 @@ if __name__ == "__main__":
     print(vehicle1.position)
     print(vehicle2.position)
 
-    BoundaryCondition().check_condition("periodic",vehicle2)
+    BoundaryCondition().check_condition("open",vehicles)    
 
+    print(vehicles)
     print(vehicle1.position)
     print(vehicle2.position)
