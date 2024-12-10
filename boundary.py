2d460a1471851799e3c03c0c3d1044b7e38fa400
motsu13 <130522059+motsu13@users.noreply.github.com>
2024-12-10 13:13:43 +0900

    boundaryで車両位置を周期境界条件の時正しい位置に置くプログラムを作成

--- boundary.py
+++ boundary.py
@@ -2,7 +2,7 @@ import vehicle
 import setting
 
 
-class BoundaryCondition():
+class BoundaryCondition:
     def __init__(self):
         pass
 
@@ -12,23 +12,23 @@ class BoundaryCondition():
         condition:境界条件(periodic,open)
         """
         if condition == "periodic":
-            vehicle.position = vehicle.position % setting.Const.ROAD_LENGTH
+            vehicle.position = vehicle.position % setting.Const().ROAD_LENGTH
 
 if __name__ == "__main__":
     vehicles = []
-    vehicle1 = vehicle.Vehicle(3,5,104)
-    vehicle2 = vehicle.Vehicle(104,5,3)
+    vehicle1 = vehicle.Vehicle(3,5,100)
+    vehicle2 = vehicle.Vehicle(100,5,3)
 
-    print(type(vehicle.Vehicle.position))  # 'int' 型であるべき
-    print(type(setting.Const.ROAD_LENGTH))  # 
-
-    BoundaryCondition().check_condition("periodic",vehicle1)
+    print(type(vehicle1.position))  # 'int' 型であるべき
+    print(type(setting.Const().ROAD_LENGTH))  # 
     vehicles.append(vehicle1)
     vehicles.append(vehicle2)
 
-    
+    print(vehicles)
+    print(vehicle1.position)
+    print(vehicle2.position)
 
-print(vehicles)
-print(vehicle1.position)
-print(vehicle2.position)
+    BoundaryCondition().check_condition("periodic",vehicle2)
 
+    print(vehicle1.position)
+    print(vehicle2.position)
