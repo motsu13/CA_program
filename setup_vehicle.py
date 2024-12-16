0240f6021fe2c730bfa4d60ecfb56a6380de4e55
motsu13 <130522059+motsu13@users.noreply.github.com>
2024-12-16 16:34:42 +0900

    車両の初期配置

--- setup_vehicle.py
+++ setup_vehicle.py
@@ -2,7 +2,8 @@ import vehicle
 import setting
 import numpy as np
 """
-道路を生成し、道路上にに車両を生成する。
+道路を生成し、道路上にに車両を生成する。道路は一次元のリストとして示される。
+道路上では車両は速度の値として示され、-1は車両が存在しないものとして示す
 """
 
 class SetupVehicle:
@@ -12,21 +13,26 @@ class SetupVehicle:
     def setup_vehicles(self,num_vehicle):
         """
         指定された数の車両を道路にランダムに配置します。
-        num_vehicles: 車両の数
+        num_vehicles: 車両数
         """
-        self.vehicles = []
-        self.road = np.full(setting.Const().ROAD_LENGTH,-1)
+        self.vehicles = []# 車両リスト
+        self.road = np.full(setting.Const().ROAD_LENGTH,-1) #道路
 
-        # #車両を一様分布でランダムな位置に配置
+        # 車両の位置を一様分布で選択して配置
         initial_positions = np.random.choice(setting.Const().ROAD_LENGTH,num_vehicle,replace=False)
-        for pos in initial_positions:
+        
+        # 得られた位置を元に車両を道路上に配置
+        for i in range(len(initial_positions)):
             speed = 0
-            veh = vehicle.Vehicle(pos,speed,)
-            self.road[pos] = speed
+            veh = vehicle.Vehicle(initial_positions[i],
+                                  speed,initial_positions[(i+1)%len(initial_positions)])
+            self.vehicles.append(veh)
+            self.road[initial_positions[i]] = speed
 
 
 if __name__ == "__main__":
-    initial = SetupVehicle.setup_vehicles(10)
+    initial = SetupVehicle()
+    initial.setup_vehicles(10)
     print(initial.road)
-
+    print(initial.vehicles)
 
